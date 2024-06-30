ee5f66ec56617f1c4b8f2e7069f24ee63f6ef7bd
samikshyananda4 <samikshya.nanda.4848@gmail.com>
2024-06-30 19:20:27 +0530

    ignition

--- App.js
+++ App.js
@@ -1,11 +1,12 @@
-
+import React from "react";
+import ReactDOM from "react-dom/client";
 // nester structures
 const parent=React.createElement(
     "div", 
     { id:"parent"},[
     React.createElement("div", { id:"child"}, 
-       [React.createElement("h1",{id:"h1tag"},"I am H1 Tag"),
-        React.createElement("h2", {id:"h1tag"}, "Another H2 Tag (sibling)")
+       [React.createElement("h1",{id:"h1tag"},"This is Samikshya Nanda btw"),
+        React.createElement("h2", {id:"h1tag"}, "Another H2 Tag (sibling) some random stuffs over here for eduvational purpose")
        ]), React.createElement("div", {id:"child"}, 
         [React.createElement("h1",{id:"h1tag"},"I am H1 Tag"),
          React.createElement("h2", {id:"h1tag"}, "Another H2 Tag (sibling)")
@@ -20,4 +21,5 @@ const parent=React.createElement(
        const container= document.getElementById("root")
        const root=ReactDOM.createRoot(container);
 //        root.render(head) 
-   root.render(parent)
+       root.render(parent)
+       
--- README.md
+++ README.md
@@ -1,1 +1,21 @@
 Reactive is a git repository of me learning react from different sources and pushing it to evaluate my own growth and showcase my journey of learning to those who are interested for the same.
+
+#Parcel-Usage
+-dev Build 
+-Local Server
+-HMR(refreshing page)-HOT MODULE REPLACEMENT
+-Uses a File Watching Algorithms, faster development experience because its also doing cacheing
+-Image Optimization
+-Minification
+-Bundling because it is a bundler in the first place
+-Compress so the size is small
+-Ship it on to production
+-Consistent Hashing 
+-Code Splitting
+-Differencial Bundling-to support older browsers
+-Host on HTTPs
+-Tree SHAKING Algorithm -rmove unused code for you ,suppose there is some extra random code
+-Different Build for the development and production
+
+
+
